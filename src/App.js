89e492c519ac0d48a5c02b874cf419be7363b9b7
hannahdavantes <hannahabigail.davantes@edu.sait.ca>
2020-11-18 08:45:20 -0700

    Added gh-pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
+class App extends Component {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+      searchField: ''
+    };
+
+    //bind() -> returns a new functionw here the context of this is set to whatever we passed to it
+    //for every class method that is written -> we must bind it
+    //there is a way to easy fix it by using arrow function
+    // this.handleChange = this.handleChange.bind(this);
+  }
+
+  //when component mounts -> renders it to page
+  //calls whatever block of code
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }))
+  }
+
+  //bind() -> returns a new functionw here the context of this is set to whatever we passed to it
+  //for every class method that is written -> we must bind it
+  //there is a way to easy fix it by using arrow function
+  // handleChange(e) {
+  //   //JavaScript doesn't set context
+  //   this.setState({ searchField: e.target.value })
+  // }
+  //arrow functions automatically binds to the place they were defined
+  handleChange = (e) => {
+    //JavaScript doesn't set context
+    this.setState({ searchField: e.target.value })
+  }
+
+  render() {
+
+    //destructuring -> allows us to pull properties off of an object and set it as constants
+    const { monsters, searchField } = this.state;
+
+    //similar to these:
+    // const monsters = this.state.monsters;
+    // const searchField = this.state.searchField;
+
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    )
+
+    return (
+      <div className="App">
+        <h1> Monsters Rolodex </h1>
+        
+        <SearchBox
+          placeholder='search monsters'
+          handleChange={this.handleChange}
+        />
+
+        <CardList monsters={filteredMonsters} />
+
+      </div>
+    );
+  }
 }
 
 export default App;
